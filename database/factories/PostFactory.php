703f9cb09dca9007fe1426f87d2750121db7f542
Dmitry <bootleggerlow@gmail.com>
2020-03-28 02:45:13 +0300

    'init'

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    $title = $faker->realText(40,5);
+    $short_title = Str::length($title) > 30 ? mb_substr($title, 0, 30) . '...' : $title;
+    $created = $faker->dateTimeBetween('-30 days','-1 days');
+    $descr = $faker->realText(100,5);
+    return [
+        'title' => $title,
+        'short_title' => $short_title,
+        'author_id'=>rand(1,4),
+        'descr'=>$descr,
+        'created_at'=>$created,
+        'updated_at'=>$created,
+    ];
+
+});
---
+++ database/migrations/2020_03_27_213826_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id('post_id');
+            $table->bigInteger('author_id')->unsigned()->index();
+            $table->string('title');
+            $table->string('short_title');
+            $table->string('img')->nullable();
+            $table->text('descr');
+            $table->timestamps();
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
